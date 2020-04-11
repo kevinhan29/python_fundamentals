dbabee4a76a4e325dd86c9dfd4939b866b765fef
Kevin Han <kevinhan@fb.com>
2020-04-11 13:19:18 -0700

    upload generators exercise

--- 15_generators/15_02_divisible.py
+++ 15_generators/15_02_divisible.py
@@ -3,3 +3,10 @@ Create a Generator that loops over the given list and prints out only
 the items that are divisible by 1111.
 
 '''
+
+x = [5, 1111, 9, 0, 8888, 983, 50938239, 192838, 9301, 9999]
+
+gen = (i for i in x if i%1111 == 0)
+
+for i in gen:
+    print(i)
--- 15_generators/15_03_floor.py
+++ 15_generators/15_03_floor.py
@@ -4,3 +4,10 @@ Adapt your Generator expression from the previous Exercise
 What numbers do you get?
 
 '''
+x = [5, 1111, 9, 0, 8888, 983, 50938239, 192838, 9301, 9999]
+
+gen = (i // 1111 for i in x)
+
+for num in gen:
+    print(num)
+
