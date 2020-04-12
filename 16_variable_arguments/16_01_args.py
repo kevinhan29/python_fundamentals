a0bc2c3a08c10cc9cad39032df69b9c3c3c3ed64
Kevin Han <kevinhan@fb.com>
2020-04-12 16:21:57 -0700

    upload variable arguments

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,14 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+''
+
+def adder(*args):
+    sum_ = args[0]
+    for i in range(1, len(args)):
+        sum_ = sum_ + args[i]
+    return sum_
+
+print(adder(2,4,5,9,3))
+
+print(adder("Hello", " ", "World"))
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,24 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def math_operator(*args, **kwargs):
+    temp = args[0]
+    for i in range(1, len(args)):
+        if kwargs["operator"] == "+":
+            temp = temp + args[i]
+        elif kwargs["operator"] == "-":
+            temp = temp - args[i]
+        elif kwargs["operator"].lower() == "x":
+            temp = temp * args[i]
+        elif kwargs["operator"] == "/":
+            temp = temp / args[i]
+        else:
+            return "Invalid Operator"
+    return temp
+
+print(math_operator(1, 2, 3, operator = "+"))
+print(math_operator(1, 2, 3, operator = "-"))
+print(math_operator(1, 2, 3, operator = "X"))
+print(math_operator(1, 2, 3, operator = "/"))
+print(math_operator(1, 2, 3, operator = "hi"))
