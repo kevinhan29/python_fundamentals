cf3ed3bb0051ef2009f60f295dcbad101d036bc4
Kevin Han <kevinhan@fb.com>
2020-04-13 19:54:57 -0700

    upload decorators lab

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -2,3 +2,14 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+
+def html_tag(func):
+    def wrapper(func):
+        return(f"<p>{func}</p>")
+    return wrapper
+
+@html_tag
+def some_text(m):
+    return m
+
+print(some_text("CodingNomads"))
--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,14 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+def html_tagger(func):
+    def wrapper(*args):
+        return(f"<{args[1]}>{args[0]}</{args[1]}>")
+    return wrapper
+
+@html_tagger
+def some_text(text, tag):
+    pass
+
+print(some_text("CodingNomads", "html"))
