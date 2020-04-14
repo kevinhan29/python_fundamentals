4ad5ff66d820bdab926ab5526fe639231027612f
Kevin Han <kevinhan@fb.com>
2020-04-13 21:27:37 -0700

    Upload lambdas lab

--- 18_lambdas/18_01_first_lambda.py
+++ 18_lambdas/18_01_first_lambda.py
@@ -2,3 +2,7 @@
 Write a lambda function that takes in 3 numbers and returns the sum of the numbers.
 
 '''
+
+sum_3 = lambda x, y, z: x + y + z
+
+print(sum_3(1,2,3))
--- 18_lambdas/18_04_hello.py
+++ 18_lambdas/18_04_hello.py
@@ -4,3 +4,8 @@ Create a lambda expression that takes no input and prints "hello world" to the c
 What does it return?
 
 '''
+
+hello = lambda : print("hello world")
+
+x = hello()
+print(x)
